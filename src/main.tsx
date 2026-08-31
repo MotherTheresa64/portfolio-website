b6aeaae83c4d6aca4728685d3d2e7711eccc0f36
Noah Ragan <64329247+MotherTheresa64@users.noreply.github.com>
2026-08-31 08:08:49 -0500

    Load viewport-fit flagship layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "./spacing-fix.css";
+import "./modal-fit.css";
 import { ThemeProvider } from "./context/ThemeProvider";
 
 const normalizeResumeText = () => {
