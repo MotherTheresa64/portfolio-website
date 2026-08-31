762622896f5f5f30cdbe6a73bcf99531d00a0afb
Noah Ragan <64329247+MotherTheresa64@users.noreply.github.com>
2026-08-31 05:39:36 -0500

    Render Aegis capstone before selected projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Flagship from "./components/Flagship";
 import Projects from "./components/Projects";
 import About from "./components/About";
 import Skills from "./components/Skills";
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <main>
         <Hero />
+        <Flagship />
         <Projects />
         <About />
         <Skills />
