52963851fb3054d3a2349b2f38a59b5bca4f5759
Noah Ragan <64329247+MotherTheresa64@users.noreply.github.com>
2026-09-01 02:28:11 -0500

    Load refined checkpoint geometry

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./styles.css";
 import "./circuit-motion.css";
 import "./projects-fork.css";
+import "./checkpoint-layout.css";
 import "./mobile.css";
 
 createRoot(document.getElementById("root")!).render(
