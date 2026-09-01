ece3d0c0ad72e434220e8ac06c9ddb1dd6b893cf
Noah Ragan <64329247+MotherTheresa64@users.noreply.github.com>
2026-09-01 02:15:59 -0500

    Load centered Projects Root fork layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./circuit-motion.css";
+import "./projects-fork.css";
 import "./mobile.css";
 
 createRoot(document.getElementById("root")!).render(
